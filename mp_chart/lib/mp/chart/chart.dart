a952725a1bed43a637efa4bdac7b3b27f8c9354e
Sameer Maharjan <34150612+firefoxqb@users.noreply.github.com>
2021-05-12 11:49:48 +0545

    Remove capture in chart

--- mp_chart/lib/mp/chart/chart.dart
+++ mp_chart/lib/mp/chart/chart.dart
@@ -32,31 +32,6 @@ abstract class ChartState<T extends Chart> extends State<T> {
 
   void updatePainter();
 
-  void capture() async {
-    if (isCapturing) return;
-    isCapturing = true;
-    String directory = "";
-    if (Platform.isAndroid) {
-      directory = (await getExternalStorageDirectory()).path;
-    } else if (Platform.isIOS) {
-      directory = (await getApplicationDocumentsDirectory()).path;
-    } else {
-      return;
-    }
-
-    String fileName = DateTime.now().toIso8601String();
-    String path = '$directory/$fileName.png';
-    _screenshotController.capture(path: path, pixelRatio: 3.0).then((imgFile) {
-      ImageGallerySaver.saveImage(Uint8List.fromList(imgFile.readAsBytesSync()))
-          .then((value) {
-        imgFile.delete();
-      });
-      isCapturing = false;
-    }).catchError((error) {
-      isCapturing = false;
-    });
-  }
-
   @override
   void didUpdateWidget(T oldWidget) {
     super.didUpdateWidget(oldWidget);
